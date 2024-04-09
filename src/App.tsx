3f92970d51f5b07578d679da6a9ceff4bb8e9957
tiagofsant <f.santos_aer@hotmail.com>
2024-04-09 01:26:43 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Stack, Typography } from "@mui/material";
+import "./App.css";
+
+function App() {
+  return (
+    <Stack>
+      <Typography variant="h1">Olá, mundo!</Typography>
+    </Stack>
+  );
+}
+
+export default App;
